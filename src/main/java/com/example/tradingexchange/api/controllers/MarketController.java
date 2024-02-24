bdd06c1e72ece1b2cae5408a1cc2b2544cf4f22d
saarang1995 <saarang.tiwari@hotmail.com>
2024-02-24 11:52:01 +0000

    feat: add getCurrencyPairById api

--- src/main/java/com/example/tradingexchange/api/controllers/MarketController.java
+++ src/main/java/com/example/tradingexchange/api/controllers/MarketController.java
@@ -3,10 +3,12 @@ package com.example.tradingexchange.api.controllers;
 import com.example.tradingexchange.api.models.CurrencyPair;
 import com.example.tradingexchange.api.services.MarketService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("market")
@@ -22,6 +24,11 @@ public class MarketController {
     @GetMapping("currency-pairs")
     public List<CurrencyPair> getCurrencyPairs() {
         return this.marketService.getCurrencyPairs();
+    }
+
+    @GetMapping("currency-pairs/{id}")
+    public Optional<CurrencyPair> getCurrencyPairById(@PathVariable Long id) {
+        return this.marketService.getCurrencyPairById(id);
     }
 
 }
--- src/main/java/com/example/tradingexchange/api/services/MarketService.java
+++ src/main/java/com/example/tradingexchange/api/services/MarketService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MarketService {
@@ -20,5 +21,9 @@ public class MarketService {
 
     public List<CurrencyPair> getCurrencyPairs() {
         return this.currencyPairRepository.findAll();
+    }
+
+    public Optional<CurrencyPair> getCurrencyPairById(Long id) {
+        return this.currencyPairRepository.findById(id);
     }
 }
